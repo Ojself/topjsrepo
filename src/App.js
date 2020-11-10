b54331ec42eabd1e4b91312d66750d3860ac7ff6
Tormod Flesjø <tormod.flesjo@gmail.com>
2020-11-10 14:30:51 +0100

    Refactor code and add styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ghlogo from "./ghlogo.png";
+// import ghlogo from "../assets/ghlogo.png";
 
 import "./App.css";
 import Repositories from "./components/Repositories";
 import Pagination from "./components/Pagination";
 
-const REPO_PER_PAGE = 20;
+const REPOS_PER_PAGE = 20;
 const URL =
   "https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100";
 
@@ -19,7 +19,7 @@ const App = () => {
   // Renders error to front page and prints error message if fetch fails
   const errHandler = (error) => {
     console.error(`An error occured:\n${error}`);
-    setErrorMessage(JSON.stringify(error));
+    setErrorMessage(JSON.stringify(error.message));
     setTimeout(() => {
       setErrorMessage("");
     }, 8000);
@@ -37,30 +37,26 @@ const App = () => {
     getRepos();
   }, []);
 
-  const indexOfLastRepo = currentPage * REPO_PER_PAGE;
-  const indexOffirstRepo = indexOfLastRepo - REPO_PER_PAGE;
+  const indexOfLastRepo = currentPage * REPOS_PER_PAGE;
+  const indexOffirstRepo = indexOfLastRepo - REPOS_PER_PAGE;
   const currentRepos = repos.slice(indexOffirstRepo, indexOfLastRepo);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const mainContent = loading ? (
-    <img src={ghlogo} className="spinner-loading" alt="loading" />
+    <img src={`${process.env.PUBLIC_URL}/assets/ghlogo.png`} id="spinner-loading" alt="loading" />
   ) : (
     <>
-      <Pagination
-        currentPage={currentPage}
-        reposPerPage={REPO_PER_PAGE}
-        totalRepos={repos.length}
-        paginate={paginate}
-      />
+                <p class="lead mb-0 ">Most popular Github repositories written in mainly javascript ⭐️</p>
+
       <Repositories
-        reposPerPage={REPO_PER_PAGE}
+        reposPerPage={REPOS_PER_PAGE}
         currentPage={currentPage}
         repos={currentRepos}
       />
       <Pagination
         currentPage={currentPage}
-        reposPerPage={REPO_PER_PAGE}
+        reposPerPage={REPOS_PER_PAGE}
         totalRepos={repos.length}
         paginate={paginate}
       />
--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -13,39 +13,47 @@ const Repositories = ({ reposPerPage, currentPage, repos }) => {
     } = repo;
     const listPosition = (currentPage - 1) * reposPerPage + i + 1;
     return (
-      <tr key={id} title={description}>
-        <th scope="col">{listPosition}</th>
-        <td>
+      <tr className="d-flex" key={id} title={description}>
+        <th className="col-2" scope="col">{listPosition}</th>
+        <td className="col-5">
           <a id="url" href={html_url} rel="noreferrer" target="_blank">
             {name}
           </a>
         </td>
-        <td>{stargazers_count.toLocaleString()}</td>
-        <td>{created_at.slice(0, 4)}</td>
+        <td className="col-3">{stargazers_count.toLocaleString()}</td>
+        <td className="col-2">{new Date(created_at).getFullYear()}</td>
       </tr>
     );
   });
 
+  
+
+  const ghStarSvg =(<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg>)
+
   return (
-    <table className="table table-striped">
+    <div className="table-responsive bg-white rounded shadow my-5">
+    <table className="table table-fixed">
       <thead>
-        <tr>
-          <th className="w-15" scope="col">
+        <tr className="d-flex">
+          <th className="col-2" scope="col">
             #
           </th>
-          <th className="w-50" scope="col">
+          <th className="col-5" scope="col">
             Name
           </th>
-          <th className="w-25" scope="col">
-            Stars
+          <th className="col-3 pl-4" scope="col">
+            {ghStarSvg}
           </th>
-          <th className="w-30" scope="col">
+          <th className="col-2" scope="col">
             Created
           </th>
         </tr>
       </thead>
       <tbody>{repoList}</tbody>
     </table>
+    </div>
   );
 };
 
